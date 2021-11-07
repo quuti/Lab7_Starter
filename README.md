0abc97a917eca9b0bc76a40ed293c521184affe6
Andrew Park <ajpark@ucsd.edu>
2021-11-07 14:50:46 -0800

    copied files from partner for my submission

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 7 - Starter Code
+Hao Yuan
+Andrew Park
--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -11,7 +11,7 @@
   *     easily modify the URL without refreshing the page or anything
   */
 
-export class Router {
+ export class Router {
   static routes = {};
 
   /**
@@ -38,6 +38,7 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+    this[page] = pageFunc;
   }
 
   /**
@@ -65,5 +66,21 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+
+    if(this[page] == undefined){
+      console.log("This function does not exist");
+      return;
+    }
+    var hash;
+    if (page == 'home') {
+      hash = '';
+    } else {
+      hash = '#' + page;
+    }
+    if (statePopped != true && (window.location.hash != hash)) {
+      var currentState = {'page': page};
+      history.pushState(currentState, '', window.location.origin+hash);
+    }
+    this[page]();
   }
 }
